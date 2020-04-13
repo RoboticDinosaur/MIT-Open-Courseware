22f7d5ad1081ef6ef6c152aa6286ca904454f7fb
Robert Forbes <robert@roboticdinosaur.co.uk>
2020-04-13 17:14:09 +0100

    changed to numpy as per the homework... stupid

--- 60001 Introduction to Computer Science and Programming in Python/ps0/ps0.py
+++ 60001 Introduction to Computer Science and Programming in Python/ps0/ps0.py
@@ -1,7 +1,8 @@
+import numpy
 import math
 
 x = int(input("Enter number x: "))
 y = int(input("Enter number y: "))
 
 print("X**y = ", x ** y)
-print("log(x) = ", int(math.log(x, 2)))
+print("log(x) = ", math.log(x))
