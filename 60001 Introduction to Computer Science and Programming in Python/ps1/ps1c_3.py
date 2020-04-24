bd2fca697975792cea8bf7b44fab6566ae313dba
Robert Forbes <robert@roboticdinosaur.co.uk>
2020-04-24 13:01:07 +0100

    Changed comment to single line

--- 60001 Introduction to Computer Science and Programming in Python/ps1/ps1c_3.py
+++ 60001 Introduction to Computer Science and Programming in Python/ps1/ps1c_3.py
@@ -1,33 +1,21 @@
-""" 
-def main():
-    annual_salary = int(input('Enter your annual salary: '))
-    portion_save_percent = float(input('Enter the percent of your salary to save, as a decimal: '))
-    total_cost = int(input('Enter the cost of your dream home: '))
-    semi_annual_raise = float(input('Enter your semi annual raise: '))
-
-    portion_down_payment = total_cost * 0.25
-    savings = 0
-
-    r = float(0.04)
-
-    month = int(0)
-
-    while savings < portion_down_payment:
-        if month % 6 == 0 and month != 0:
-            annual_salary += annual_salary * semi_annual_raise
-
-        monthly_salary = annual_salary / 12
-
-
-        portion_saved = monthly_salary * portion_save_percent
-        roi = float(savings * r / 12)
-
-        savings += roi + portion_saved
-        month += 1
-
-
-    print('Number of months: ', month)
- """
+# def main():
+#     annual_salary = int(input('Enter your annual salary: '))
+#     portion_save_percent = float(input('Enter the percent of your salary to save, as a decimal: '))
+#     total_cost = int(input('Enter the cost of your dream home: '))
+#     semi_annual_raise = float(input('Enter your semi annual raise: '))
+#     portion_down_payment = total_cost * 0.25
+#     savings = 0
+#     r = float(0.04)
+#     month = int(0)
+#     while savings < portion_down_payment:
+#         if month % 6 == 0 and month != 0:
+#             annual_salary += annual_salary * semi_annual_raise
+#         monthly_salary = annual_salary / 12
+#         portion_saved = monthly_salary * portion_save_percent
+#         roi = float(savings * r / 12)
+#         savings += roi + portion_saved
+#         month += 1
+#     print('Number of months: ', month)
 
 def main():
     annual_salary = int(input('Enter your annual salary: '))
