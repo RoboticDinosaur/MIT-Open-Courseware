46e8fa6800b71ffb23aed1ce421dad7c765e189d
Robert Forbes <robert@roboticdinosaur.co.uk>
2020-06-04 20:55:45 +0100

    TEMP COMMIT

--- 6.005 - Software Construction/03. Classes and Objects/Classes/Card.java
+++ 6.005 - Software Construction/03. Classes and Objects/Classes/Card.java
@@ -1,5 +1,19 @@
 public class Card {
-    public Card() {
+
+    private int value;
+    private int suit;
+
+    public Card(int value, int suit) {
+        // Print it out
+        // Create a card
+        // Compare a card
         
+    }
+    public int getValue() {
+        return value;
+    }
+
+    public int getSuit() {
+        return suit;
     }
 }
