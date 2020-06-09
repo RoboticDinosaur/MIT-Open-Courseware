f96107b03af0c75f0e6730577c82f78e72e09f89
Robert Forbes <robert@roboticdinosaur.co.uk>
2020-06-09 06:59:58 +0100

    Fuck it all. I hate this section.

--- 6.005 - Software Construction/03. Classes and Objects/Enum Types/Card.java
+++ 6.005 - Software Construction/03. Classes and Objects/Enum Types/Card.java
@@ -1,51 +1,75 @@
+/*
+ * Copyright (c) 1995, 2008, Oracle and/or its affiliates. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *   - Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *
+ *   - Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in the
+ *     documentation and/or other materials provided with the distribution.
+ *
+ *   - Neither the name of Oracle or the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+ * IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */ 
+
 public class Card {
+    private final Rank rank;
+    private final Suit suit;
 
-    private int value;
-    private int suit;
+    // Kinds of suits
+    // public final static int DIAMONDS = 1;
+    // public final static int CLUBS    = 2;
+    // public final static int HEARTS   = 3;
+    // public final static int SPADES   = 4;
+    //
+    // Moved to Suit.java
 
-    public enum value {
-        1(1),
-        2(2),
-        3(3),
-        4(4),
-        5(5),
-        6(6),
-        7(7),
-        8(8), 
-        9(9), 
-        10(10), 
-        J(11),
-        Q(12), 
-        K(13)
-    }
-    public enum suits {
-        DIAMONDS, CLUBS, SPADES, HEARTS
-    }
 
-    public Card(int value, int suit) {
-        this.value = value;
+    // Kinds of ranks
+    // public final static int ACE   = 1;
+    // public final static int DEUCE = 2;
+    // public final static int THREE = 3;
+    // public final static int FOUR  = 4;
+    // public final static int FIVE  = 5;
+    // public final static int SIX   = 6;
+    // public final static int SEVEN = 7;
+    // public final static int EIGHT = 8;
+    // public final static int NINE  = 9;
+    // public final static int TEN   = 10;
+    // public final static int JACK  = 11;
+    // public final static int QUEEN = 12;
+    // public final static int KING  = 13;
+    //
+    // MOVED TO Rank.java because it is enum Class
+
+    public Card(Rank rank, Suit suit) {
+        this.rank = rank;
         this.suit = suit;
     }
 
-
-    public int getValue() {
-        return value;
-    }
-
-    public int getSuit() {
+    public Suit getSuit() {
         return suit;
     }
-/*
-    public boolean compareValue(char compareValue) {
-        return true;
-    }
 
-    public boolean compareSuit(char compareSuit) {
-        return true;
+    public Rank getRank() {
+        return rank;
     }
-
-    public boolean compare(String card) {
-        return true;
-    }
-    */
 }
+
