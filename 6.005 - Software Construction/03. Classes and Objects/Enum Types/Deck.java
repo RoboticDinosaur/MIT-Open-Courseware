132ec666c43bf30b6128d902ad25e43d12d7e0a4
Robert Forbes <robert@roboticdinosaur.co.uk>
2020-06-09 07:00:07 +0100

    Ugh

--- 6.005 - Software Construction/03. Classes and Objects/Enum Types/Deck.java
+++ 6.005 - Software Construction/03. Classes and Objects/Enum Types/Deck.java
@@ -31,13 +31,13 @@
 
 import java.util.*;
 
-public class Deck3 {
-    private static Card3[] cards = new Card3[52];
-    public Deck3() {
+public class Deck {
+    private static Card[] cards = new Card[52];
+    public Deck() {
         int i = 0;
         for (Suit suit : Suit.values()) {
             for (Rank rank : Rank.values()) {
-                cards[i++] = new Card3(rank, suit);
+                cards[i++] = new Card(rank, suit);
             }
         }
     }
